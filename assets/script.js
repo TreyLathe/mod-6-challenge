17b31911612ecad171ddc5e9d9be67cad949f67f
WCIII <warrenlathe@me.com>
2024-01-05 18:19:30 -1000

    updated html and css files. added some variables to js that will need

--- assets/script.js
+++ assets/script.js
@@ -5,4 +5,15 @@ function dateTimeUpdate() {
   dateTime.textContent = rightNow;
 }
 
+
 setInterval(dateTimeUpdate, 1000) ;
+
+
+let apiKey = '8c92ac2f6bf9c17163d39d5544b8a5f9';
+let searchForm = document.getElementById('searchForm');
+let cityInput = document.getElementById('cityInput');
+let cityList = document.getElementById('cityList');
+let currentWeatherSection = document.getElementById('currentWeather');
+let fiveDayForecastSection = document.getElementById('fiveDayForecast');
+
+
